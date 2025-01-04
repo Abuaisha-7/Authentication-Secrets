a6639e8e379d52d78e9a1ca0bc57574f0e0b8f13
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-01-04 05:50:47 -0800

    Level 4 - Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5"); // md5 (message-digest algorithm) is a hashing algorithm that is commonly used to hash passwords
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
-console.log("weak password hash: " + md5("123456"));
-console.log("strong password hash: " + md5("sjkhdfsd8f7jhsd$%$sdfsdfHJKHSJFHDSF78324"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -39,33 +38,37 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUser.save();
-    User.find() 
-        .then(function(users){
-            res.render("secrets", {usersWithSecrets: users});
-        })
-        .catch(function(err){
-            console.log(err);
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
         });
+    
+        newUser.save()
+            .then(function(){
+                res.render("secrets", {usersWithSecrets: [newUser]});
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+    });
    
 });
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username})
         .then(function(foundUser){
     
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets", {usersWithSecrets: [foundUser]});
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render("secrets", {usersWithSecrets: [foundUser]});
+                    }
+                });
             }
         
          })
